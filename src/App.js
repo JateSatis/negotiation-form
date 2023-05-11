ee74836ed19cf6a97b05f88c28e9d23e270cc583
Maxim Danilov <JateSatis@gmail.com>
2023-05-11 22:47:06 +0300

    Changed routing to work with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 
 import Form from "./components/Form.js";
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/negotiation-form">
+      <Routes>
+        <Route element={<App />} path="/" />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
